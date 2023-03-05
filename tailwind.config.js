5b00332767c717538f74fd100e1d2c638450df25
Stewart Michaels <steward.kama@gmail.com>
2023-03-05 18:54:37 +0530

    mobile version
     half done mobile version

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        mybgcolor: '#1c1c1c',
+        myyellow: '#FFE002',
+        almostyellow: '#B28228',
+      },
+      fontFamily: {
+        open: ['Open Sans', 'san-serif'],
+        sri: ['Sriracha', 'cursive'],
+      },
+    },
+  },
+  plugins:[],
+}

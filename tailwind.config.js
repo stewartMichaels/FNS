5d1ec4b733c529b72b751a25194487a58ed7bf04
Stewart Michaels <steward.kama@gmail.com>
2023-03-06 20:58:32 +0530

    home page
    almost done with home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       colors: {
         mybgcolor: '#1c1c1c',
         myyellow: '#FFE002',
+        myotheryellow: '#FFDB15',
         almostyellow: '#B28228',
+        myviolet: '#8A6FDF',
+        mygreen: '#A8E10C',
+        mypink: '#FF5765',
       },
       fontFamily: {
         open: ['Open Sans', 'san-serif'],
